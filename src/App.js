ccc203ed8705e6cdd102f5b9b13a9c009dcafd06
akhileshtiwari2 <akhilesh.tiwari02@gmail.com>
2023-05-17 11:18:26 +0530

    Dark Mode: Alert Function Added

--- src/App.js
+++ src/App.js
@@ -2,23 +2,35 @@ import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import "./App.css";
 import React, { useState } from "react";
+import Alert from "./components/Alert";
 
 function App() {
   const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+  };
 
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
-      document.body.style.backgroundColor = '#808080';
+      document.body.style.backgroundColor = "#808080";
+      showAlert("Dark mode has been enabled", "success");
     } else {
       setMode("light");
-      document.body.style.backgroundColor = 'white';
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
     }
   };
 
   return (
     <>
       <Navbar aboutText="About us" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container">
         <TextForm heading="Enter the text below" mode={mode} />
       </div>
